b90aaefd0161afd3ad526bdce96f3419109eb91e
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2020-05-05 23:46:00 +0700

    a

---
+++ linhtinh/DrawHeart/heart.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<windows.h>
+
+
+void VeTraiTim()
+{
+	printf("                     .:*\"\"\"*:..:*\"\"\"*:.         .:*\"\"\"*:..:*\"\"\"*:.     \n");		Sleep(250);
+	printf("                     *........I.......*         *........I.......*                 \n");		Sleep(250);
+	printf("                     \"......Love......\"         \"......Love......\"             \n");		Sleep(250);
+	printf("                       \".....You....\"             \".....You....\"               \n");		Sleep(250);
+	printf("                         \"........\"                 \"........\"                 \n");		Sleep(250);
+	printf("                           \"....\"                     \"....\"                   \n");		Sleep(250);
+	printf("                             **                         **                         \n\n\n");    Sleep(300);
+	printf("	*      *       x * x   *       * ******      *   *   x * x    *     *          \n");		Sleep(300);
+	printf("	|      |      *     *   \\     /  |            \\ /   *     *   *     *        \n");		Sleep(300);
+	printf("	*      *     x       x   *   *   ******        *   x       x  *     *          \n");		Sleep(300);
+	printf("	|      |      x     x     \\ /    |             |    x     x   *.   .*         \n");		Sleep(300);
+	printf("	*      ******  * x *       *     ******        *     * x *      ***            \n\n\n");    Sleep(1250);
+	system("cls");
+}
+
+int main()
+{
+	int x = 0;
+	while (x <= 10)
+	{
+		VeTraiTim();
+		x++;
+	}
+	system("pause");
+}
+
---
+++ linhtinh/DrawHeart/heartcolorful.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<windows.h>
+
+void VeTraiTim()
+{
+
+	printf("                       .:*\"\"\"\"\"*:.         .:*\"\"\"\"\"*:.                   .:*\"\"\"\"\"*:.         .:*\"\"\"\"\"*:.  \n");
+	printf("                      *...........*       *...........*                 *...........*       *...........*                     \n");
+	printf("                     \".............\\     /.............\"               \".............\\     /.............\"              \n");
+	printf("                     \"..............*. .*..............\"               \"..............*. .*..............\"                \n");
+	printf("                      \"...............*...............\"                 \"...............*...............\"                 \n");
+	printf("                       \".............................\"                   \".............................\"                  \n");
+	printf("                         \".........................\"                       \".........................\"                    \n");
+	printf("                           \".....................\"                           \".....................\"                      \n");
+	printf("                             \".................\"                               \".................\"                        \n");
+	printf("                               \".............\"                                   \".............\"                          \n");
+	printf("                                 \".........\"                                       \".........\"                            \n");
+	printf("                                   \".....\"                                           \".....\"                              \n"); 
+	printf("                                     *.*                                               *.*                                    \n\n\n");
+	printf("          *       *     *     * * * *    * * * *    *     *                                             \n");
+	printf("          |       |    / \\    |       *  |       *   \\   /                                            \n");
+	printf("          * ----- *   *---*   * * * *    * * * *       *                                                \n");
+	printf("          |       |  /     \\  |          |             |                                               \n");
+	printf("          *       * *       * *          *             *                                                \n\t\t\t\t\t\t\t\t\t\t\t/\n");
+	printf("                                *       *       *   x * x    *.    .*  ******  *    *  /  .*****.           \n");
+	printf("                                 \\     / \\     /   *     *   |  \\ / |  |       | \\  |    *                  \n");
+	printf("                                  *   *   *   *   x       x  *   *  *  ******  *  * *     '*****.             \n");
+	printf("                                   \\ /     \\ /     x     x   |      |  |       |   \\|    .      .*             \n");
+	printf("                                    *       *       * x *    *      *  ******  *    *     ******                  \n\n");
+	printf("                                                                                  * * * .       *     *   *           \n");
+	printf("                                                                                  |      *     / \\     \\ /          \n");
+	printf("                                                                                  *      *    * - *     *             \n");
+	printf("                                                                                  |      *   /     \\    |            \n");
+	printf("                                                                                  * * * '   *       *   *             ");   Sleep(500);
+	system("cls"); 
+}
+
+int main()
+{
+	vetraitim:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		VeTraiTim();
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
+		VeTraiTim();
+	goto vetraitim;
+}
+
